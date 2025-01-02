366c3d4249ecb271efb711aef31906674ec3a535
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2025-01-02 22:58:29 +0200

    sign up

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,58 +1,65 @@
 import React from 'react'
 const Signup = () => {
   return (
-   <section className="bg-mainBg  pt-[4%]">
+   <section className="bg-mainBg  py-[4%] ">
  <form className='wrapper flex flex-col justify-center items-center gap-6'>
   <img src="./null.png" alt="profile-pic" className="block size-[180px] cursor-pointer" />
-  <div className='flex flex-col gap-2'>
+  <div className='flex flex-col gap-2 w-full'>
     <label htmlFor="username">Name</label>
     <input
       type="text"
       name="username"
-      placeholder="Enter your name here!"
+      placeholder="John Doe"
       id="username" 
+      className='bg-transparent border border-[#4B5768] focus:outline-none rounded-md pl-4 py-3'
     />
   </div>
-  <div className='flex flex-col gap-2'>
+  <div className='flex flex-col gap-2 w-full'>
     <label htmlFor="address">Address</label>
     <input
       type="text"
       name="address"
-      placeholder="Enter your address here!"
+      placeholder="Palestine"
       id="address"
+      className='bg-transparent border border-[#4B5768] focus:outline-none rounded-md pl-4 py-3'
     />
   </div>
-  <div className='flex flex-col gap-2'>
-    <label htmlFor="email">Email</label>
+  <div className='flex flex-col gap-2 w-full'>
+    <label htmlFor="email">Email Address</label>
     <input
       type="email"
       name="email"
-      placeholder="Enter your valid email here!"
+      placeholder="johndoe@gmail.com"
       id="email"
+      className='bg-transparent border border-[#4B5768] focus:outline-none rounded-md pl-4 py-3'
+
     />
   </div>
-  <div className='flex flex-col gap-2'>
+  <div className='flex flex-col gap-2 w-full'>
     <label htmlFor="password">Password</label>
     <input
       type="password"
       name="password"
       placeholder="Enter your password here!"
       id="password"
+      className='bg-transparent border border-[#4B5768] focus:outline-none rounded-md pl-4 py-3'
     />
   </div>
-  <div className='flex flex-col gap-2'>
+  <div className='flex flex-col gap-2 w-full'>
     <label htmlFor="cPassword">Confirm Password</label>
     <input
       type="password"
       name="cPassword"
       placeholder="Confirm your password here!"
       id="cPassword"
+      className='bg-transparent border border-[#4B5768] focus:outline-none rounded-md pl-4 py-3'
+
     />
   </div>
-  <p className="already-account">
-          Already have an account? <a href="/login">Log In</a>
+  <p >
+          Already have an account? <a href="/login" className='text-mainColor font-medium' >Log In</a>
         </p>
-  <button type="submit" className='SignUpBtn'> Sign Up</button>
+  <button type="submit" className='bg-mainColor text-white w-full rounded-md px-30 py-3'> Sign Up</button>
 </form>
 
    </section>
